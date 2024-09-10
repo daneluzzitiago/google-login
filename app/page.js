b03111938d41e9ab09a1d4ed87d74736f0b2406c
Tiago Daneluzzi <daneluzzitiago@gmail.com>
2024-09-10 11:26:31 -0300

    navigate between account creation and login

--- app/page.js
+++ app/page.js
@@ -1,18 +1,38 @@
+"use client"
+
 import { Box, Button, Card, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
 export default function Home() {
+  const [displayCreateAccount, setDisplayCreateAccount] = useState(false);
+
+  const loginComponent = () =>
+    <>
+      <Typography>Google Login Integration</Typography>
+      <TextField label="E-mail"></TextField>
+      <TextField label="Password"></TextField>
+      <Button variant="contained">Login</Button>
+      <Button onClick={() => setDisplayCreateAccount(true)}>Create account</Button>
+    </>
+  
+  const createAccountComponent = () =>
+    <>
+      <Typography>New Account</Typography>
+      <TextField label="E-mail"></TextField>
+      <TextField label="Password"></TextField>
+      <Button variant="contained">Create</Button>
+      <Button onClick={() => setDisplayCreateAccount(false)}>Go back</Button>
+    </>
+
   return (
     <Box display="flex" height="90vh" alignItems="center" justifyContent="center">
       <Card>
         <Box display="flex" flexDirection="column" m={2}>
           <Stack spacing={2}>
-          <Typography align="center">
-            Google Login Integration
-          </Typography>
-          <TextField label="E-mail"></TextField>
-          <TextField label="Password"></TextField>
-          <Button variant="contained">Login</Button>
-          <Button>Create account</Button>
+            {displayCreateAccount
+              ? createAccountComponent()
+              : loginComponent()
+            }
           </Stack>
         </Box>
       </Card>
